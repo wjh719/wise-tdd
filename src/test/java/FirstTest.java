8d747098b6e88d5fae494f1202d9960c07b439ab
wjh719 <https://github.com/wjh719>
2025-01-06 18:38:24 +0900

    work2

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FirstTest {
+
+
+    @Test
+    void t1(){
+
+        int rst = 1;
+
+        assertThat(rst).isEqualTo(1);
+    }
+}
