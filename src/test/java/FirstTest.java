5ea50b0cdf2f70b39016b15ec64a1f76d11c8e65
wjh719 <https://github.com/wjh719>
2025-01-08 15:18:33 +0900

    test6 red

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -11,7 +11,7 @@ public class FirstTest {
 
 
     @Test
-    void t1(){
+    void t1() {
 
         int rst = 1;
 
@@ -19,7 +19,7 @@ public class FirstTest {
     }
 
     @Test
-    void t2(){
+    void t2() {
         TestApp app = new TestApp();
         app.run();
 
@@ -29,20 +29,30 @@ public class FirstTest {
     }
 
     @Test
-    void t3(){
+    void t3() {
         String out = TestBot.run("종료");
+        assertThat(out).contains("명언앱을 종료합니다.");
 
         // 출력값을 체크
     }
 
     @Test
     @DisplayName("앱 시작시 '== 명언 앱 ==' 출력")
-    void t4(){
+    void t4() {
         String out = TestBot.run("종료");
 
-        assertThat(out.toString())
+        assertThat(out)
                 .containsSubsequence("== 명언 앱 ==", "명언앱을 종료합니다.");
 
         // 출력값을 체크
+    }
+
+    @Test
+    @DisplayName("등록 - 명언 1개 입력")
+    void t5() {
+        String out = TestBot.run("등록\n현재를 사랑하라.\n작자미상\n종료");
+
+        assertThat(out)
+                .containsSubsequence("명연 : ", "작가 : ");
     }
 }
