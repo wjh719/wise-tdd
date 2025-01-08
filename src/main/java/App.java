518b3c95c7b84e5d2d208c1a2055a03baa55c9e2
wjh719 <https://github.com/wjh719>
2025-01-08 22:57:12 +0900

    test6 green

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,19 +11,21 @@ public class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
-        while(true) {
-            System.out.println("명령 ) ");
-            String cmd = sc.nextLine();
+        while (true) {
 
-            if(cmd.equals("종료")) {
+            System.out.println("명령 ) ");
+            String cmd = sc.nextLine(); //
+
+            if (cmd.equals("종료")) {
+                System.out.println("명언앱을 종료합니다.");
                 break;
+
+            } else if(cmd.equals("등록")) {
+
+                System.out.println("명언 : ");
+                System.out.println("작가 : ");
+                System.out.println("1번 명언이 등록되었습니다.");
             }
         }
-
-        System.out.println("명언 : ");
-        System.out.println("작가 : ");
-
-        System.out.println("명언앱을 종료합니다.");
     }
 }
-
