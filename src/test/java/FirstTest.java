3f27a3036b45be9baeb9ab0094d5a1ea4c972fec
wjh719 <https://github.com/wjh719>
2025-01-08 15:05:38 +0900

    work6

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -30,16 +30,7 @@ public class FirstTest {
 
     @Test
     void t3(){
-        // 테스트 봇 선입력
-        Scanner sc = new Scanner("종료\n");
-
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-
-        TestApp app = new TestApp();
-        app.run();
-
-        assertThat(out.toString()).contains("명언앱을 종료합니다.");
+        String out = TestBot.run("종료");
 
         // 출력값을 체크
     }
@@ -47,17 +38,7 @@ public class FirstTest {
     @Test
     @DisplayName("앱 시작시 '== 명언 앱 ==' 출력")
     void t4(){
-        // 테스트 봇 선입력
-        Scanner sc = new Scanner("종료\n");
-
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-
-        TestApp app = new TestApp();
-        app.run();
-
-        // 테스트 코드 작성 어려움.
-        // assertJ 사용법을 잘 모름.
+        String out = TestBot.run("종료");
 
         assertThat(out.toString())
                 .containsSubsequence("== 명언 앱 ==", "명언앱을 종료합니다.");
