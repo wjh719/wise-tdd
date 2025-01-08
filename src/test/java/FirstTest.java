d11b3eaaa8d2754e16e023af6c71ea7f7781997c
wjh719 <https://github.com/wjh719>
2025-01-08 12:33:55 +0900

    work5

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -37,6 +38,30 @@ public class FirstTest {
 
         TestApp app = new TestApp();
         app.run();
+
+        assertThat(out.toString()).contains("명언앱을 종료합니다.");
+
+        // 출력값을 체크
+    }
+
+    @Test
+    @DisplayName("앱 시작시 '== 명언 앱 ==' 출력")
+    void t4(){
+        // 테스트 봇 선입력
+        Scanner sc = new Scanner("종료\n");
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        TestApp app = new TestApp();
+        app.run();
+
+        // r -> 실패하는 테스트 케이스
+        assertThat(out.toString())
+                .contains("==명언 앱==")
+                .contains("명언앱을 종료합니다.");
+
+
 
         assertThat(out.toString()).contains("명언앱을 종료합니다.");
 
